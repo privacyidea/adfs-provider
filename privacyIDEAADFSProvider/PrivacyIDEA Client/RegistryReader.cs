43c976aca7bb8425284c8c5734faf67532ae35c1
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2025-11-11 10:51:27 +0100

    Update RegistryReader.cs

--- privacyIDEAADFSProvider/PrivacyIDEA Client/RegistryReader.cs
+++ privacyIDEAADFSProvider/PrivacyIDEA Client/RegistryReader.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.Win32;
 
-namespace PrivacyIDEASDK
+namespace PrivacyIDEAADFSProvider.PrivacyIDEA_Client
 {
 
     public delegate void LogFunction(string message);
