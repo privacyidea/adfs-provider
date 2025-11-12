f15d9c353fe5943f1561ecbc333a9b6401cab99d
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2025-11-12 09:51:12 +0100

    Update PIConstants.cs

--- privacyIDEAADFSProvider/PrivacyIDEA Client/PIConstants.cs
+++ privacyIDEAADFSProvider/PrivacyIDEA Client/PIConstants.cs
@@ -2,7 +2,16 @@
 {
     internal class PIConstants
     {
-        public const string USER_AGENT = "User-Agent";
+        // ENDPOINTS
+        public const string AUTH_ENDPOINT = "/auth";
+        public const string POLLTRANSACTION_ENDPOINT = "/validate/polltransaction";
+        public const string TRIGGERCHALLENGE_ENDPOINT = "/validate/triggerchallenge";
+        public const string VALIDATE_INITIALIZE_ENDPOINT = "/validate/initialize";
+        public const string TOKEN_ENDPOINT = "/token/";
+        public const string TOKEN_INIT_ENDPOINT = "/token/init";
+        public const string VALIDATE_CHECK_ENDPOINT = "/validate/check";
+        public const string POST = "POST";
+        public const string GET = "GET";
 
         // SERVER RESPONSE
         public const string RESULT = "result";
@@ -31,6 +40,7 @@
         public const string PASSKEY_REGISTRATION = "passkey_registration";
         public const string LINK = "link";
         public const string WEBAUTHNSIGNREQUEST = "webAuthnSignRequest";
+        public const string CANCEL_ENROLLMENT = "cancel_enrollment";
 
         // REQUEST PARAMETERS
         public const string REALM = "realm";
@@ -38,11 +48,40 @@
         public const string USERNAME = "username";
         public const string PASS = "pass";
         public const string OTP = "otp";
+        public const string USER = "user";
+        public const string GENKEY = "genkey";
+        public const string TOTP = "totp";
+        public const string ORIGIN = "Origin";
+        public const string PASSWORD = "password";
+        public const string USER_AGENT = "User-Agent";
 
         // TOKEN TYPES
         public const string TOKEN_TYPE_OTP = "otp";
         public const string TOKEN_TYPE_PUSH = "push";
         public const string TOKEN_TYPE_PASSKEY = "passkey";
         public const string TOKEN_TYPE_WEBAUTHN = "webauthn";
+
+        // FIDO2 PARAMETERS
+        public const string CREDENTIALID = "credentialId";
+        public const string CREDENTIAL_ID = "credential_id";
+        public const string CLIENTDATA = "clientdata";
+        public const string CLIENTDATAJSON = "clientdatajson";
+        public const string CLIENTDATAJSON_CAM = "clientDataJSON";
+        public const string SIGNATUREDATA = "signaturedata";
+        public const string SIGNATURE = "signature";
+        public const string AUTHENTICATORDATA = "authenticatordata";
+        public const string AUTHENTICATORDATA_CAM = "authenticatorData";
+        public const string USERHANDLE = "userhandle";
+        public const string USERHANDLE_CAM = "userHandle";
+        public const string RAW_ID = "raw_id";
+        public const string RAWID = "rawid";
+        public const string RAWID_CAM = "rawId";
+        public const string ASSERTIONCLIENTEXTENSIONS = "assertionclientextensions";
+        public const string AUTHENTICATORATTACHMENT = "authenticatorattachment";
+        public const string AUTHENTICATORATTACHMENT_CAM = "authenticatorAttachment";
+        public const string ATTESTATIONOBJECT = "attestationobject";
+        public const string ATTESTATIONOBJECT_CAM = "attestationObject";
+
+        public const string MEDIA_TYPE_URLENCODED = "application/x-www-form-urlencoded";
     }
 }
