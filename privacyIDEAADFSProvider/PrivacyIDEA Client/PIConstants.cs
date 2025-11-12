1f860953177bdf485dd719c0aef28e992efc5a3e
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2025-11-12 08:10:41 +0100

    Update PIConstants.cs

--- privacyIDEAADFSProvider/PrivacyIDEA Client/PIConstants.cs
+++ privacyIDEAADFSProvider/PrivacyIDEA Client/PIConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PrivacyIDEAADFSProvider.PrivacyIDEA_Client
+﻿namespace PrivacyIDEAADFSProvider.PrivacyIDEA_Client
 {
     internal class PIConstants
     {
